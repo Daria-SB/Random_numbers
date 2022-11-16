9dde180f4950212615ba29e9cd5d571f26c07afb
Daria SB <d.sasbojarska@gmail.com>
2022-11-16 12:32:38 +0100

    cyferki

---
+++ cyferki_by_D.py
@@ -0,0 +1,16 @@
+import random
+
+random_int = random.randint(0,10)
+input_int = None
+counter = 0
+while random_int != input_int:
+    input_int = int(input("Proszę wprowadzić liczbę od 0 do 101: "))
+    if  random_int == input_int:
+        print("Brawo!")
+    elif input_int > random_int:
+        print("Ta liczba jest za duża, spróbuj jeszcze raz!")
+    elif input_int < random_int:
+        print("Ta liczba jest za mała, spróbuj jeszcze raz!")
+    counter += 1
+
+print(f"Udało Ci się zgadnąć w {counter} próbach")
